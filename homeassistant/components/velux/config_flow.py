1cdef7550ca12ad44e14b28437dc2afb97a7e476
Paul Daumlechner <paul.daumlechner@live.de>
2024-03-05 20:46:02 +0000

    update config_flow

--- homeassistant/components/velux/config_flow.py
+++ homeassistant/components/velux/config_flow.py
@@ -43,12 +43,13 @@ class VeluxConfigFlow(ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST]})
-            for host in self.hosts:
-                if user_input[CONF_HOST] == host.ip_address:  # type: ignore[union-attr]
-                    await self.async_set_unique_id(host.hostname)  # type: ignore[union-attr]
-                    self._abort_if_unique_id_configured(
-                        updates={CONF_HOST: host.ip_address}  # type: ignore[union-attr]
-                    )
+            if self.hosts is not None:
+                for host in self.hosts:
+                    if user_input[CONF_HOST] == host.ip_address:  # type: ignore[union-attr]
+                        await self.async_set_unique_id(host.hostname)  # type: ignore[union-attr]
+                        self._abort_if_unique_id_configured(
+                            updates={CONF_HOST: host.ip_address}  # type: ignore[union-attr]
+                        )
 
             pyvlx = PyVLX(
                 host=user_input[CONF_HOST], password=user_input[CONF_PASSWORD]
